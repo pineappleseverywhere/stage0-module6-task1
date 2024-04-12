325bf430a8f999eae698a731b8ecef9bd9030c17
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-11 22:29:48 -0400

    yo

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,29 @@
 package com.epam.mjc.stage0;
 
-public class Animal {
 
+class Main{
+    public static void main(String[] args) {
+        Animal animal = new Animal("red", 4, false);
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+        System.out.println(animal.getDescription());
+    }
+}
+public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription(){
+        String pawOrPaws = numberOfPaws == 1? "paw" : "paws";
+        String fur = hasFur? "a" : "no";
+
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " "
+                + pawOrPaws + " and " + fur + " fur.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,20 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
+    public Bird(){
+        super("blue", 2, false);
+    }
 
+    @Override
+    public String getDescription() {
+
+        return super.getDescription() + " Moreover, it has 2 wings and can fly.";
+    }
+    /*
+    Override getDescription() method for class Bird: add one more sentence to the
+    description. The result must be "This animal is mostly blue. It has 2 paws and
+     no fur. Moreover, it has 2 wings and can fly."
+(Optional) Create an object of each class and call getDescription() method for both
+ of them. Try to explain the output results.
+     */
 }
